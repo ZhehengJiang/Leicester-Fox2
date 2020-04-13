1ce170ca9665a4b38e2fdecdc06380928b789aff
ZhehengJiang <zj53@leicester.ac.uk>
2020-04-13 14:04:14 +0100

    Physionet_2020 first submission

---
+++ network.py
@@ -0,0 +1,134 @@
+from keras import backend as K
+from keras.layers import CuDNNGRU,Bidirectional,Lambda
+
+def _bn_relu(layer, dropout=0, **params):
+    from keras.layers import BatchNormalization
+    from keras.layers import Activation
+    layer = BatchNormalization()(layer)
+    layer = Activation(params["conv_activation"])(layer)
+
+    if dropout > 0:
+        from keras.layers import Dropout
+        layer = Dropout(params["conv_dropout"])(layer)
+
+    return layer
+
+def add_conv_weight(
+        layer,
+        filter_length,
+        num_filters,
+        subsample_length=1,
+        **params):
+    from keras.layers import Conv1D
+    layer = Conv1D(
+        filters=num_filters,
+        kernel_size=filter_length,
+        strides=subsample_length,
+        padding='same',
+        kernel_initializer=params["conv_init"])(layer)
+    return layer
+
+def resnet_block(
+        layer,
+        num_filters,
+        subsample_length,
+        block_index,
+        **params):
+    from keras.layers import Add 
+    from keras.layers import MaxPooling1D
+    from keras.layers.core import Lambda
+
+    def zeropad(x):
+        y = K.zeros_like(x)
+        return K.concatenate([x, y], axis=2)
+
+    def zeropad_output_shape(input_shape):
+        shape = list(input_shape)
+        assert len(shape) == 3
+        shape[2] *= 2
+        return tuple(shape)
+
+    shortcut = MaxPooling1D(pool_size=subsample_length)(layer)
+    zero_pad = (block_index % params["conv_increase_channels_at"]) == 0 \
+        and block_index > 0
+    if zero_pad is True:
+        shortcut = Lambda(zeropad, output_shape=zeropad_output_shape)(shortcut)
+
+    for i in range(params["conv_num_skip"]):
+        if not (block_index == 0 and i == 0):
+            layer = _bn_relu(
+                layer,
+                dropout=params["conv_dropout"] if i > 0 else 0,
+                **params)
+        layer = add_conv_weight(
+            layer,
+            params["conv_filter_length"],
+            num_filters,
+            subsample_length if i == 0 else 1,
+            **params)
+    layer = Add()([shortcut, layer])
+    return layer
+
+def get_num_filters_at_index(index, num_start_filters, **params):
+    return 2**int(index / params["conv_increase_channels_at"]) \
+        * num_start_filters
+
+def add_resnet_layers(layer, **params):
+    layer = add_conv_weight(
+        layer,
+        params["conv_filter_length"],
+        params["conv_num_filters_start"],
+        subsample_length=1,
+        **params)
+    layer = _bn_relu(layer, **params)
+    for index, subsample_length in enumerate(params["conv_subsample_lengths"]):
+        num_filters = get_num_filters_at_index(
+            index, params["conv_num_filters_start"], **params)
+        layer = resnet_block(
+            layer,
+            num_filters,
+            subsample_length,
+            index,
+            **params)
+    layer = _bn_relu(layer, **params)
+    return layer
+
+def add_output_layer(layer, **params):
+    from keras.layers.core import Dense, Activation
+    from keras.layers.wrappers import TimeDistributed
+    layer = Bidirectional(CuDNNGRU(128,  return_sequences=True, return_state=False))(layer)
+    layer = TimeDistributed(Dense(params["num_categories"]))(layer)
+    return Activation('softmax')(layer)
+
+def add_compile(model, **params):
+    from keras.optimizers import Adam
+    optimizer = Adam(
+        lr=params["learning_rate"],
+        clipnorm=params.get("clipnorm", 1))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=optimizer,
+                  metrics=['accuracy'])
+
+def pre_process(x,mean,std):
+    # mean = params['mean']
+    # std = params['std']
+    x = (x - mean) / std
+    return x
+
+def build_network(**params):
+    from keras.models import Model
+    from keras.layers import Input
+    inputs = Input(shape=params['input_shape'],
+                   dtype='float32',
+                   name='inputs')
+    # mean = K.constant(params['mean'])
+    # std = K.constant(params['std'])
+    processed_inputs = Lambda(pre_process,arguments={'mean':params['mean'],'std':params['std']})(inputs)
+    layer = add_resnet_layers(processed_inputs, **params)
+
+    output = add_output_layer(layer, **params)
+    model = Model(inputs=[inputs], outputs=[output])
+    if params.get("compile", True):
+        add_compile(model, **params)
+    return model
--- run_12ECG_classifier.py
+++ run_12ECG_classifier.py
@@ -2,30 +2,61 @@
 
 import numpy as np
 import joblib
+import network
+import json
+from sklearn.preprocessing import normalize
 from get_12ECG_features import get_12ECG_features
 
 def run_12ECG_classifier(data,header_data,classes,model):
 
+    net_classes = ['AF', 'AF,LBBB', 'AF,LBBB,STD', 'AF,PAC', 'AF,PVC', 'AF,RBBB', 'AF,STD', 'AF,STE', 'I-AVB', 'I-AVB,LBBB',
+     'I-AVB,PAC', 'I-AVB,PVC', 'I-AVB,RBBB', 'I-AVB,STD', 'I-AVB,STE', 'LBBB', 'LBBB,PAC', 'LBBB,PVC', 'LBBB,STE',
+     'Normal', 'PAC', 'PAC,PVC', 'PAC,STD', 'PAC,STE', 'PVC', 'PVC,STD', 'PVC,STE', 'RBBB', 'RBBB,PAC', 'RBBB,PAC,STE',
+     'RBBB,PVC', 'RBBB,STD', 'RBBB,STE', 'STD', 'STD,STE', 'STE']
+
     num_classes = len(classes)
+    class_to_int = dict(zip(classes, range(num_classes)))
     current_label = np.zeros(num_classes, dtype=int)
     current_score = np.zeros(num_classes)
+    # Use your classifier here to obtain a label and score for each class.
+    input_length = 68864
+    if data.T.shape[0]>input_length:
+        input_length = int(data.T.shape[0]/256+1)*256
+    input_data = np.zeros([1,input_length,data.T.shape[1]])
+    input_data[0,0:data.T.shape[0],:] = data.T
+    score = model.predict(input_data)
+    pred_scores = np.sum(score,axis=1)
+    pred_scores = pred_scores[:, 0:36]
+    pred_scores = normalize(pred_scores)
+    pred_label = pred_scores.argmax(axis=1)
+    pred_c = net_classes[pred_label[0]]
+    pred_c_split = pred_c.split(',');
+    for i in range(len(pred_c_split)):
+        current_label[class_to_int[pred_c_split[i]]] = 1
+        current_score[class_to_int[pred_c_split[i]]] = np.max(pred_scores)
+    pred_l = np.argwhere(pred_scores[0,:]>0.5)
+    if pred_l.size != 0:
+        for ii in range(len(pred_l)):
+            pred_c = net_classes[pred_l[ii][0]]
+            pred_c_split = pred_c.split(',')
+            for j in range(len(pred_c_split)):
+                current_label[class_to_int[pred_c_split[j]]] = 1
+                current_score[class_to_int[pred_c_split[j]]] = pred_scores[0,pred_l[ii][0]]
 
-    # Use your classifier here to obtain a label and score for each class. 
-    features=np.asarray(get_12ECG_features(data,header_data))
-    feats_reshape = features.reshape(1,-1)
-    label = model.predict(feats_reshape)
-    score = model.predict_proba(feats_reshape)
-
-    current_label[label] = 1
-
-    for i in range(num_classes):
-        current_score[i] = np.array(score[0][i])
+    # for i in range(num_classes):
+    #     current_score[class_to_int[pred_c_split[i]]] = np.max(pred_scores)
 
     return current_label, current_score
 
 def load_12ECG_model():
     # load the model from disk 
-    filename='finalized_model.sav'
-    loaded_model = joblib.load(filename)
+    filename = 'net_weights.h5'
+    config_file = 'config.json'
+    params = json.load(open(config_file, 'r'))
+    params.update({
+        "input_shape": [None, 12]
+    })
+    loaded_model = network.build_network(**params)
+    loaded_model.load_weights('net_weights.h5')
 
     return loaded_model
